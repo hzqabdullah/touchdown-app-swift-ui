f381904f71c4d76b62509fd5bc1d6ae112867761
hzqabdullah <hzq.abdullah@gmail.com>
2024-05-05 08:52:03 +0800

    Initial Commit

---
+++ Touchdown Swift UI/Touchdown_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Touchdown_Swift_UIApp.swift
+//  Touchdown Swift UI
+//
+//  Created by Haziq Abdullah on 05/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Touchdown_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
